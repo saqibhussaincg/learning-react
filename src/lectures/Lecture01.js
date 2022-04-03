f3b0a61af0af60f6fb5fdbe068642572eb1e3587
Saqib Hussain - Designer + Coder <saqibhussaincg@gmail.com>
2022-04-03 19:21:31 +0500

    'first'

---
+++ src/lectures/Lecture01.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const flname = 'saqib hussain';
+
+function Lecture01() {
+  return (
+    <div>
+    <h1 style={{ background : 'pink'}}>my name is {flname} </h1>
+    </div>
+  )
+}
+
+export default Lecture01
